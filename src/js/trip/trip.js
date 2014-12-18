a24c20a7d0a71eda39475d908c7576efcb23fdab
Alexander Wende <alex.wende@gmail.com>
2014-12-18 20:54:51 +0100

    rendering a trip list on module startup

--- src/js/trip/trip.js
+++ src/js/trip/trip.js
@@ -1,20 +1,36 @@
 import Module from '../module';
 import TripService from './services/trip-service';
+import TripListView from './views/trip-list-view';
 
 class Trip extends Module {
+
+    initialize (trips) {
+
+        this.trips = trips;
+
+        this.listView = new TripListView({
+            element: this.element,
+            selector: this.selector,
+            scope: {
+                items: this.trips
+            }
+        });
+
+        this.listView.render();
+    }
+
+    handleError (error) {
+
+        console.error(error);
+    }
 
     start () {
 
         super();
 
         TripService.getTrips()
-            .then(function (trips) {
-                this.trips = trips;
-                console.log(this.trips);
-            }.bind(this))
-            .catch(function (error) {
-                console.error(error);
-            }.bind(this));
+            .then(this.initialize.bind(this))
+            .catch(this.handleError.bind(this));
     }
 }
 
