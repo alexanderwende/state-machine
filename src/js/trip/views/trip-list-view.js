1ca9f49edfbf88e6a41dcd86ab071b80616f84f3
Alexander Wende <alex.wende@gmail.com>
2014-12-21 21:58:58 +0100

    view options and default options

--- src/js/trip/views/trip-list-view.js
+++ src/js/trip/views/trip-list-view.js
@@ -1,15 +1,50 @@
 import View from '../../view';
 import template from '../templates/trip-list-template.ejs!tpl';
 
+function extend (target, source, deep) {
+
+    for (let property in source) {
+        if (source.hasOwnProperty(property)) {
+            target[property] = clone(source[property], deep);
+        }
+    }
+
+    return target;
+}
+
+function clone (source, deep) {
+
+    if (typeof source === 'object') {
+        if (deep) {
+            if (source instanceof Object) {
+                return extend({}, source, deep);
+            }
+            else if (source instanceof Array) {
+                return extend([], source, deep);
+            }
+            else {
+                return new source.constructor(source);
+            }
+        }
+    }
+
+    return source;
+}
+
 export default class TripListView extends View {
 
     constructor (options) {
 
-        super(options);
+        this.defaultOptions = {
+            template: template,
+            events: [
+                {element: 'a', event: 'click', handler: '_handleClick'}
+            ]
+        };
 
-        if (!this.template) {
-            this.template = template;
-        }
+        options = extend(this.defaultOptions, options);
+
+        super(options);
     }
 
     onRender () {
