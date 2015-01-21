49f9d19d14e0191976522d725f812294f76df915
Alexander Wende <alex.wende@gmail.com>
2015-01-21 20:18:35 +0100

    working on module states, nested states and router

--- src/js/app.js
+++ src/js/app.js
@@ -75,6 +75,8 @@ import Application from './app/app';
 //
 //    app.state.enterState('home');
 
+
+
     class StateMachine {
 
         constructor (options) {
@@ -82,32 +84,229 @@ import Application from './app/app';
             this.states = {};
 
             this.current = null;
+            this.default = null;
         }
 
-        transition (fromState, toState, params) {
+        transition (state, params) {
 
-            if (fromState !== toState) {
+            if (this.current !== state) {
 
-                fromState.exit();
+                this.exitState(this.current);
 
-                toState.enter(params);
+                this.enterState(state, params);
+            }
+        }
+
+        exitState (state) {
+
+            if (this.states[state]) {
+                this.states[state].exit();
+            }
+        }
+
+        enterState (state, params) {
+
+            if (this.states[state]) {
+                this.states[state].enter(params);
+                this.current = state;
+            }
+            else if (this.default) {
+                this.default.enter(params);
+                this.current = this.default;
+            }
+        }
+
+        state (id, options) {
+
+            if (options) {
+
+                options.id = id;
+
+                let state = new State(options);
+
+                this.states[id] = state;
+
+                if (state.default) {
+                    this.default = state;
+                }
+
+                return this;
+            }
+
+            return this.states[id];
+        }
+    }
+
+
+
+    class State {
+
+        constructor (options) {
+
+            this.id         = options.id;
+            this.default    = options.default ? true : false;
+
+            this.data       = {};
+
+            this._enter     = options.enter;
+            this._exit      = options.exit;
+        }
+
+        enter (params) {
+
+            if (this._enter) {
+
+                this._enter(params);
+            }
+        }
+
+        exit () {
+
+            if (this._exit) {
+
+                this._exit();
             }
         }
     }
+
+
 
     class Module extends StateMachine {
 
         constructor (options) {
 
             super(options);
+
+            this.element = options ? options.element || document.body : document.body;
+
+            this.modules = {};
+
+            this.isStarted = false;
+        }
+
+        transition (state, params) {
+
+            super(state, params);
+
+            console.log('state: %s', this.current);
+            console.log('state params: %o', params);
+        }
+
+        state (id, options) {
+
+            if (options) {
+
+                options.enter   = options.enter ? options.enter.bind(this) : undefined;
+                options.exit    = options.exit ? options.exit.bind(this) : undefined;
+            }
+
+            return super(id, options);
+        }
+
+        module (id, module) {
+
+            if (module) {
+
+                module.id = id;
+
+                this.modules[id] = module;
+
+                return this;
+            }
+
+            return this.modules[id];
+        }
+
+        start () {
+
+            if (this.isStarted) { return; }
+
+            this.isStarted = true;
+
+            this.transition();
+        }
+
+        stop () {
+
+            if (!this.isStarted) { return; }
+
+            this.isStarted = false;
         }
     }
 
+
+
     class Router {
 
-        constructor (options) {}
+        constructor (options) {
+
+            this.routes = [];
+
+            this.isStarted = false;
+
+            this._onHashChange = function (event) {
+                // get the new url from the hashchange event
+                var match = event.newURL.match(/#(.+)$/);
+                // and extract the hash fragment
+                var hash = match && match[1] || '';
+                // update the router instance to the new hash
+                console.log(hash);
+            }.bind(this);
+        }
 
         navigate (url) {}
+
+        start () {
+
+            if (this.isStarted) { return; }
+
+            if ('onhashchange' in window) {
+                window.addEventListener('hashchange', this._onHashChange);
+            }
+
+            this.isStarted = true;
+        }
+
+        stop () {
+
+            if (!this.isStarted) { return; }
+
+            if ('onhashchange' in window) {
+                window.removeEventListener('hashchange', this._onHashChange);
+            }
+
+            this.isStarted = false;
+        }
+    }
+
+
+
+    class Users extends Module {
+
+        constructor (options) {
+
+            super(options);
+
+            this.state('list', {
+                    default: true,
+                    enter: function (params) {
+                        this.element.innerHTML = '<h1>User List</h1>';
+                    },
+                    exit: function () {}
+                })
+                .state('details', {
+                    enter: function (params) {
+                        this.element.innerHTML = `<h1>User Details: ${params.user}</h1>`;
+                    },
+                    exit: function () {}
+                })
+                .state('edit', {
+                    enter: function (params) {
+                        this.element.innerHTML = `<h1>Edit User Details: ${params.user}</h1>`;
+                    },
+                    exit: function () {}
+                });
+        }
     }
 
     class App extends Module {
@@ -116,20 +315,36 @@ import Application from './app/app';
 
             super(options);
 
-            this.states = {
+            this.module('users', new Users());
 
-                'home': {
+            this.state('home', {
+                    default: true,
+                    enter: function (params) {
+                        this.element.innerHTML = '<h1>Home</h1>';
+                    },
+                    exit: function () {}
+                })
+                .state('about', {
+                    enter: function (params) {
+                        this.element.innerHTML = '<h1>About</h1>';
+                    },
+                    exit: function () {}
+                })
+                .state('users', {
                     enter: function (params) {},
                     exit: function () {}
-                },
-
-                'about': {
-                    enter: function (params) {},
-                    exit: function () {}
-                }
-            };
+                });
         }
     }
 
+    window.router = new Router();
+
+    window.router.start();
+
+    window.app = new App({
+        element: document.getElementById('app')
+    });
+
+    app.start();
 
 })();
